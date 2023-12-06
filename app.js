8681779c5a78161cac0c94c15b41a8d840336d9b
CECILE DORANGE <cecile.dorange45@gmail.com>
2023-12-06 09:23:19 +0100

    ajout JS fonction création ligne tableau et MAJ CSS ajout des photos

--- app.js
+++ app.js
@@ -15,3 +15,37 @@ document.addEventListener('DOMContentLoaded', function () {
         sidenav.classList.remove('active');
     }
 });
+
+/* partie JS cécile début*/
+const rang = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+const userName = ["jean","paul","pierre","lola","bob"]
+const score= [12,15,50,60,80]
+
+function createTable (rang,userName, score) {
+
+ const tr = document.createElement("tr");
+
+ const tdRang = document.createElement("td");
+ tdRang.innerText =`${rang}`
+ tr.appendChild(tdRang);
+
+ const tdUserName = document.createElement("td");
+ tdUserName.innerText =`${userName}`
+ tr.appendChild(tdUserName);
+
+ const tdScore = document.createElement("td");
+ tdScore.innerText =`${score} pts`
+ tr.appendChild(tdScore);
+
+ document.getElementById('tableScore').appendChild(tr);
+}
+
+for (let i = 0; i < rang.length;i++){
+    if (userName[i] !== undefined) {
+    createTable(rang[i],userName[i],score[i])
+    } else {
+        breack
+    }
+}
+
+/* partie JS cécile fin*/
