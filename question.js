9c8d83b01f80e268a5b750f1c56f24ec04fe1627
CecDor <150656709+CecDorWEB@users.noreply.github.com>
2023-12-07 10:26:50 +0100

    Update question.js
    correction []

--- question.js
+++ question.js
@@ -1,6 +1,6 @@
 const question = [
     {
-        name: "Question 1 - Est-ce que tu as bien dormi ?"
+        name: "Question 1 - Est-ce que tu as bien dormi ?",
         answers: [
             {
                 text: "Oui comme un loir, j’ai l’habitude de me coucher tôt !",
@@ -21,10 +21,8 @@ const question = [
               text: "Je suis insomniaque chronique",
                 gif: "https://media.giphy.com/media/cl2PsDEg80omBDkq8G/giphy.gif",
               point: 5
-          },
-          advice:
-          "Il est recommandé de dormir au moins 7 heures par nuit. Suit le lien pour trouver des conseils : https://institut-sommeil-vigilance.org/10-recommandations-de-nos-medecins-du-sommeil-pour-bien-dormir/"
-          ],  
+          } ],
+          advice: "Il est recommandé de dormir au moins 7 heures par nuit. Suit le lien pour trouver des conseils : https://institut-sommeil-vigilance.org/10-recommandations-de-nos-medecins-du-sommeil-pour-bien-dormir/",
     },
     {
         name: "Question 2 - Dis moi ce que tu manges, je te dirais si c’est bien ? ",
@@ -48,9 +46,8 @@ const question = [
               text: "Exclusivement de burgers mais y a de la salade, ça équilibre !",
                 gif: "https://media.giphy.com/media/iZjoLdS1nwMRq/giphy.gif",
               point: 10
-          },
-            advice:"conseil pour bien manger : Il est recommandé de manger équilibré et environ 5 fruits et légumes par jour : https://www.mangerbouger.fr/l-essentiel/les-recommandations-sur-l-alimentation-l-activite-physique-et-la-sedentarite"
-          ],  
+          }],
+            advice:"conseil pour bien manger : Il est recommandé de manger équilibré et environ 5 fruits et légumes par jour : https://www.mangerbouger.fr/l-essentiel/les-recommandations-sur-l-alimentation-l-activite-physique-et-la-sedentarite",  
     },{
         name: "Question 3 - Tu n’arrives toujours pas à saisir les fonctions d’ordre supérieur sur Javascript, quel est ton mood ?",
         answers: [
@@ -73,9 +70,8 @@ const question = [
               text: "Les ordres de fonction supér- quoi?",
                 gif: "https://i.pinimg.com/originals/20/db/ee/20dbeed1f5d4da547203d893a1d079b1.jpg",
               point: 5
-          },
-            advice:"Conseil : Une attitude positive vous permet de vous détendre, de vous souvenir, de vous concentrer et d'absorber les informations tout en apprenant. Vous êtes prêt à accueillir de nouvelles expériences et à reconnaître de nombreux types d'opportunités d'apprentissage. Et quand on voit des opportunités, l’espoir augmente :https://medium.com/@chrisplusone/the-benefits-of-positive-thinking-for-students-6510ce1b9a2b"
-          ],   
+          }],
+            advice:"Conseil : Une attitude positive vous permet de vous détendre, de vous souvenir, de vous concentrer et d'absorber les informations tout en apprenant. Vous êtes prêt à accueillir de nouvelles expériences et à reconnaître de nombreux types d'opportunités d'apprentissage. Et quand on voit des opportunités, l’espoir augmente :https://medium.com/@chrisplusone/the-benefits-of-positive-thinking-for-students-6510ce1b9a2b",   
 },
 {
         name: "Question 4 - Quel sportif est-tu?",
@@ -99,9 +95,8 @@ const question = [
               text: "Pfffffff la réponse me fatigue déjà",
                 gif: "https://media.giphy.com/media/8hXXilmk33wtmAGJNu/giphy.gif",
               point: 5
-          },
-            advice:"https://toutpourmasante.fr/se-mettre-au-sport-9-conseils-pour-bien-commencer/"
-          ],  
+          }],
+            advice:"https://toutpourmasante.fr/se-mettre-au-sport-9-conseils-pour-bien-commencer/",  
     },
     {
         name: "Question 5 - Continuer a sortir",
@@ -125,11 +120,9 @@ const question = [
               text: "Quoi? le covid c'est terminé?",
               gif: "https://media.giphy.com/media/3oz8xVhDcU1txAYI1i/giphy.gif",
               point: 5
-          },
-            advice:"Conseil du jour: sors tous les jours pour t'aérer l'esprit " 
-          ],  
+          }],
+            advice:"Conseil du jour: sors tous les jours pour t'aérer l'esprit ",  
     },
-    
   ]
   
 export default question; 
